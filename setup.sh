2fce9aa8e67e35b53f2981300f953ae898f0f147
Nishantrana45 <rananishu9876@gmail.com>
2026-03-26 13:59:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-mkdir -p /app/nltk_data
-python -c "import nltk; nltk.download('punkt', download_dir='/app/nltk_data'); nltk.download('stopwords', download_dir='/app/nltk_data')"
+python3.11 -c "import nltk; nltk.download('punkt', download_dir='/app/nltk_data'); nltk.download('stopwords', download_dir='/app/nltk_data')"
