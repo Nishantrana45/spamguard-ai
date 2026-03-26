fe1c5559f890b9247ca0703ddd0b9e7fd2f4cd2a
NISHANT <rananishu8630@gmail.com>
2026-03-26 12:27:59 +0530

    initial commit

---
+++ setup.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/nltk_data
+python -c "import nltk; nltk.download('punkt', download_dir='~/nltk_data'); nltk.download('punkt_tab', download_dir='~/nltk_data'); nltk.download('stopwords', download_dir='~/nltk_data')"
